0b32108489615c1f4439069ec7e17f41c863248e
Sean Russell <ser@ser1.net>
2008-10-09 21:21:15 -0400

    Initial import

---
+++ src/org/ser/timetracker/TimeTracker.java
@@ -0,0 +1,20 @@
+package org.ser.timetracker;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.Button;
+
+public class TimeTracker {
+    public static final String AUTHORITY = "net.ser1.provider.TimeTracker";
+    
+    /** Called when the activity is first created.
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+        Button start_button = (Button)findViewById(R.id.start_button);
+        //Intent projectList = new Intent(Intent.ACTION_VIEW, Uri.parse("content://org.ser1.net.ProjectList"));
+        //startActivity(projectList);
+    }
+    */
+}
